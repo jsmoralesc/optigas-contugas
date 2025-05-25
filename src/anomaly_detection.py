e6e797e03684ca609d61c1d0ea4cdd2c8d4d5c61
ikteran <karineteranenriquez@gmail.com>
2025-05-25 05:46:15 -0500

    Update anomaly_detection.py

--- src/anomaly_detection.py
+++ src/anomaly_detection.py
@@ -1,35 +1,35 @@
 import sqlite3
 import pandas as pd
-from sklearn.ensemble import IsolationForest
+from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_score
 import numpy as np
 
 DB_PATH = "db/optigas.db"
 Cliente ={
-    "CLIENTE1":{'mf':0.8,'nt':200,'cnt':0.01},
-    "CLIENTE2":{'mf':0.8,'nt':50,'cnt':0.01},
-    "CLIENTE3":{'mf':0.8,'nt':100,'cnt':0.01},
-    "CLIENTE4":{'mf':0.8,'nt':200,'cnt':0.01},
-    "CLIENTE5":{'mf':0.8,'nt':500,'cnt':0.01},
-    "CLIENTE6":{'mf':0.8,'nt':500,'cnt':0.01},
-    "CLIENTE7":{'mf':0.8,'nt':100,'cnt':0.01},
-    "CLIENTE8":{'mf':0.5,'nt':100,'cnt':0.1},
-    "CLIENTE9":{'mf':0.8,'nt':500,'cnt':0.05},
-    "CLIENTE10":{'mf':0.8,'nt':200,'cnt':0.01},
-    "CLIENTE11":{'mf':0.5,'nt':500,'cnt':0.01},
-    "CLIENTE12":{'mf':0.8,'nt':50,'cnt':0.01},
-    "CLIENTE13":{'mf':0.8,'nt':100,'cnt':0.05},
-    "CLIENTE14":{'mf':0.5,'nt':500,'cnt':0.01},
-    "CLIENTE15":{'mf':0.8,'nt':100,'cnt':0.05},
-    "CLIENTE16":{'mf':0.5,'nt':500,'cnt':0.1},
-    "CLIENTE17":{'mf':0.8,'nt':200,'cnt':0.01},
-    "CLIENTE18":{'mf':0.8,'nt':100,'cnt':0.01},
-    "CLIENTE19":{'mf':0.8,'nt':50,'cnt':0.1},
-    "CLIENTE20":{'mf':0.8,'nt':200,'cnt':0.01},
+    "CLIENTE1":{'eps':0.5,'min_samp':5},
+    "CLIENTE2":{'eps':0.5,'min_samp':5},
+    "CLIENTE3":{'eps':0.5,'min_samp':5},
+    "CLIENTE4":{'eps':0.5,'min_samp':5},
+    "CLIENTE5":{'eps':0.5,'min_samp':5},
+    "CLIENTE6":{'eps':0.5,'min_samp':5},
+    "CLIENTE7":{'eps':0.5,'min_samp':5},
+    "CLIENTE8":{'eps':0.5,'min_samp':5},
+    "CLIENTE9":{'eps':0.5,'min_samp':5},
+    "CLIENTE10":{'eps':0.5,'min_samp':5},
+    "CLIENTE11":{'eps':0.5,'min_samp':5},
+    "CLIENTE12":{'eps':0.5,'min_samp':5},
+    "CLIENTE13":{'eps':0.5,'min_samp':5},
+    "CLIENTE14":{'eps':0.5,'min_samp':5},
+    "CLIENTE15":{'eps':0.5,'min_samp':5},
+    "CLIENTE16":{'eps':0.5,'min_samp':5},
+    "CLIENTE17":{'eps':0.5,'min_samp':5},
+    "CLIENTE18":{'eps':0.5,'min_samp':5},
+    "CLIENTE19":{'eps':0.5,'min_samp':5},
+    "CLIENTE20":{'eps':0.5,'min_samp':5},
 }
 
-
 def entrenar_por_cliente(db_path):
+    # Realizar conexión a la BD
     conn = sqlite3.connect(db_path)
     df_all = pd.read_sql("SELECT * FROM gold_lecturas_completas", conn)
     df_all['timestamp'] = pd.to_datetime(df_all['timestamp'])
@@ -39,21 +39,29 @@ def entrenar_por_cliente(db_path):
     for cliente in Cliente.keys():
         df=df_all[df_all['cliente_id'] == cliente].copy()
         df.set_index('timestamp', inplace=True)
+
         # ---- 1. Detección por Reglas de Negocio ---- #
         df['alerta_presion'] = ~df['presion'].between(7.25, 17.4) 
         df['alerta_temperatura'] = ~df['temperatura'].between(0, 50)
+
+        # Anomalia sospechosa
         # Crear una condición para detectar cuando alguna variable es 0 y las otras > 0
         condicion_cero =(
             ((df['presion'] == 0) & (df['temperatura'] > 0) & (df['volumen'] > 0)) |  # presión es 0
             ((df['temperatura'] == 0) & (df['presion'] > 0) & (df['volumen'] > 0))   # temperatura es 0
         )  # volumen es 0
+
         # Combinar todas las alertas      
         df['alerta_reglas'] = df['alerta_presion'] | df['alerta_temperatura'] | condicion_cero
+
     # ---- 2. Detección por Modelos de ML ---- #
         features = ['Presion_scaled', 'Temperatura_scaled', 'Volumen_scaled']
-        iso = IsolationForest( n_estimators=Cliente[cliente]['nt'], contamination=Cliente[cliente]['cnt'], max_features=Cliente[cliente]['mf'], random_state=42)
-        iso_pred = iso.fit_predict(df[features])
+        model = DBSCAN( min_samples=Cliente[cliente]['min_samp'], eps=Cliente[cliente]['eps'], random_state=42)
+        dbscan_pred = model.fit_predict(df[features])
+        dbscan_labels = [1 if label == -1 else 0 for label in dbscan_labels]
         df['Anomalia_iso'] = np.where(iso_pred == -1, 1, 0)
+    
+
     # ---- 3. Severidad Combinada (Reglas + ML) ---- #
         df['severidad'] = df.apply(lambda row: 
             'Alto' if row['alerta_reglas'] == 1
@@ -63,6 +71,7 @@ def entrenar_por_cliente(db_path):
         resultados.append(df)
 
     df_resultado = pd.concat(resultados)
+    df_resultado.to_csv("data/gold/results.csv")
     df_resultado.drop(columns=['index'], inplace=True)
     df_resultado.reset_index(inplace=True)
     df_resultado.to_sql("gold_anomalias", conn, if_exists="replace", index=False)
